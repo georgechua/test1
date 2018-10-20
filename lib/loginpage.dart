136dfd21c52b6ca82d962916c951ade313d60f7f
georgechua <georgechua.ts@outlook.com>
2018-10-21 01:50:07 +0800

    v1

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -36,30 +36,32 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
          fit: StackFit.expand,
          children: <Widget>[
            new Image(
-             image: new AssetImage("images/logo.png"),
+             image: new AssetImage("images/onlylogo.png"),
              fit: BoxFit.cover,
              color: Colors.black87,
              colorBlendMode: BlendMode.darken,
            
            ),
            new Column(
-             // mainAxisAlignment: MainAxisAlignment.start,
-             
+             mainAxisAlignment: MainAxisAlignment.center,
              children: <Widget>[
-              
-              new Image(
+              /* new Image(
                  image: new AssetImage("images/logo.png"),
                  width: 500.0, height: 220.0,
                  fit:BoxFit.fill
-               ), 
+               ), */
+               
+          new Text("FinEduBot", style: new TextStyle(color: Colors.white,fontSize: 50.0,fontWeight: FontWeight.bold,),
+          ),
            new Container(
+             padding: const EdgeInsets.only(top:0.0),
              child: new Theme(
                data: new ThemeData(brightness: Brightness.dark,primarySwatch: Colors.blue, inputDecorationTheme: new InputDecorationTheme(
                  labelStyle: new TextStyle(color: Colors.blue, fontSize: 20.0))),
 
                     child: new Container(
                       //margin: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 10.0),
-                      padding: const EdgeInsets.fromLTRB(20.0,150.0,20.0,20.0),
+                      padding: const EdgeInsets.fromLTRB(30.0,0.0,0.0,30.0),
                                           child: new Column(
                         crossAxisAlignment: CrossAxisAlignment.center,
                  children: <Widget>[
@@ -83,9 +85,9 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
                         minWidth: 100.0,
                         color: Colors.blue,
                         textColor: Colors.white,
-                        child: new Text("Login"),
+                        child: new Text("Login", style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 17.0),),
                         onPressed: () => {},
-                        splashColor: Colors.lightBlueAccent,
+                        splashColor: Colors.lightBlue[900],
                         ),      
                  ],
                ),
@@ -101,11 +103,11 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
   }
 }
 
- class LogoImage extends StatelessWidget {
+/*  class LogoImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     AssetImage assetImage = AssetImage('images/logo.png');
     Image image = Image(image: assetImage);
     return new Container(child: image);
-  }
-} 
+  } 
+} */
