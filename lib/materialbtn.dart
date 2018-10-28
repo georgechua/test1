1c1a04e790f57e2b08318548ee9b965c1b884b2f
georgechua <georgechua.ts@outlook.com>
2018-10-29 01:00:12 +0800

    Update lib/materialbtn.dart

--- lib/materialbtn.dart
+++ lib/materialbtn.dart
@@ -2,21 +2,6 @@ import 'package:flutter/material.dart';
 import './loginpage.dart';
 
 
-class SendButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialButton(
-                        height: 40.0,
-                        minWidth: 100.0,
-                        color: Colors.blue,
-                        textColor: Colors.white,
-                        child: new Text("Send", style: new TextStyle(fontFamily: 'Nunito', fontSize: 17.0),),
-                        onPressed: () => {},
-                        splashColor: Colors.lightBlue[900],
-                    );
-  }
-}
-
 class RiskProfileButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
